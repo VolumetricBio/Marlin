5f88208c773a49978376b82402a8c1be7623196f
Jordan Miller <lumen@volumetricbio.com>
2019-01-19 17:22:25 -0600

    add the bed and turn off bed thermal protection to test how hot the bed can get, measured at two positions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -81,7 +81,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "2018-12-27; Volumetric, Inc. for the Lumen with Einsy and IGUS" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "2019-01-19; Volumetric, Inc. for the Lumen with Einsy and IGUS and BARICUDA firmware ON" // Who made the changes.
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
@@ -315,7 +315,7 @@
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
-#define TEMP_SENSOR_BED 0
+#define TEMP_SENSOR_BED 1
 #define TEMP_SENSOR_CHAMBER 0
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
@@ -481,7 +481,7 @@
  */
 
 #define THERMAL_PROTECTION_HOTENDS // Enable thermal protection for all extruders
-#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
+//#define THERMAL_PROTECTION_BED     // Enable thermal protection for the heated bed
 
 //===========================================================================
 //============================= Mechanical Settings =========================
@@ -1836,7 +1836,7 @@
 //#define SF_ARC_FIX
 
 // Support for the BariCUDA Paste Extruder
-//#define BARICUDA
+#define BARICUDA
 
 // Support for BlinkM/CyzRgb
 //#define BLINKM
