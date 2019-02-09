489ba884558acb8f55a89df8cbcc10d179428f2a
Jordan Miller <lumen@volumetricbio.com>
2019-02-08 23:13:40 -0600

    use y-axis for torque testing FTW

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1099,13 +1099,13 @@
 #if HAS_TRINAMIC
 
   #define R_SENSE           0.11  // R_sense resistor for SilentStepStick2130
-  #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
-  #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
+  #define HOLD_MULTIPLIER    1  // Scales down the holding current from run current
+  #define INTERPOLATE       false  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #define X_CURRENT          800  // rms current in mA. Multiply by 1.41 for peak current.
   #define X_MICROSTEPS        16  // 0..256
 
-  #define Y_CURRENT          800
+  #define Y_CURRENT          20
   #define Y_MICROSTEPS        16
 
   #define Z_CURRENT          800
