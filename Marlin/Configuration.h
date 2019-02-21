189c4102d15f27bd63c80e8d8e0cfa41b74276aa
Jordan Miller <lumen@volumetricbio.com>
2019-02-21 00:53:16 -0600

    x-axis can now go up to ~80 mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -883,7 +883,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 50
+#define X_BED_SIZE 75
 #define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
