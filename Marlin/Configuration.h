8c66505dc2883824ff7651dc41c4194e74beb3a9
Jordan Miller <lumen@volumetricbio.com>
2019-03-16 18:50:47 -0500

    autotuned PID for MillerLab silicone heater in PDMS with nichrome FTW

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -431,9 +431,15 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+//  #define DEFAULT_bedKp 10.00
+//  #define DEFAULT_bedKi .023
+//  #define DEFAULT_bedKd 305.4
+
+  // jmil MillerLab silicone heater 10 ohm nichrome, autotune for 37 ºC
+    #define DEFAULT_bedKp 45.84
+    #define DEFAULT_bedKi .39
+    #define DEFAULT_bedKd 1332.06
+
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
