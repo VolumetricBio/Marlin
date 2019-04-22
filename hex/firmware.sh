b5dfb67933ed83b0e5fe35e2544887623d511560
BG <bagrat@volumetricbio.com>
2019-04-22 00:00:58 -0500

    disabled heating

--- hex/firmware.sh
+++ hex/firmware.sh
@@ -3,5 +3,5 @@
 
 stty -F /dev/lumen-arduino speed 1200
 stty -F /dev/lumen-arduino speed 115200
-avrdude -p atmega2560 -c wiring -P /dev/lumen-arduino -b 250000 -U flash:w:Marlin.MillerLabLumen.hex:i -D
+avrdude -p atmega2560 -c wiring -P /dev/lumen-arduino -b 250000 -U flash:w:Marlin.demo.hex:i -D
 
